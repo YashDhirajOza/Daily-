d4628c60e676128e6f7775140b629b73e58948ad
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2025-03-09 16:56:12 +0530

    Create Answer_.java

---
+++ Q9/Answer_.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class Main{
+  public static void main(String[] lol){
+   Scanner obj= new Scanner(System.in);
+    int answer=  obj.nextInt();
+    for(int i=0;i<=answer;i++){
+      if (i%2==0){ Continue; }
+       if (i==answer-2){ Break; }
+    }
+  }
+}
+//Write a single program which demonstrates the usage of following 
+//keywords: 
+//i) import, ii) new, iii) break, v) continue 
+//Show how to compile and run the program in java. 
