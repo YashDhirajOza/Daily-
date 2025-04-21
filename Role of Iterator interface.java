3128c7bc60b4cba1b0a9aa088c15597c97c80759
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2025-04-21 14:54:52 +0530

    Create Role of Iterator interface.java

---
+++ Role of Iterator interface.java
@@ -0,0 +1,22 @@
+// Java collection frame work (java.util) the iterator interfacr povides a standardized mechanism to traveerse  elemtns ofa collection swquentially without expoing its internal represtation 
+
+// Interator 
+boolean hasNext()
+  // return tru if the iteration has more elements 
+  // return False if there are no more elements 
+E next()
+  // returns the next elements in the iterration 
+  // thorws NoSuchElemnetException if None remain 
+
+default void remove()
+
+// Java iterator interface of jave collectins allows us to access 
+// elments of the collecction and is used to iterate over the elements in the collection 
+  // It helps to easily retrives the elements of a collection and perform operations on each element 
+
+  // Iterator is a universal iteratro as it can be appiled to can Collection object 
+
+  //We ca only tranverse only in the forward direction usinf iterator 
+  // we can perfomm both read and remove operations 
+  public interface Iterator<E>
+  
